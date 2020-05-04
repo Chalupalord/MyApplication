4456c59c48115cdeae5368ec94276bbaf830c677
ChalupaLord <jfaler@gmail.com>
2020-05-04 12:04:40 -0600

    test 1

---
+++ app/src/main/java/com/chalupa/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.chalupa.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity as AppCompatActivity1
+
+class MainActivity : AppCompatActivity1() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var jim = "jim"
+
+    }
+}
