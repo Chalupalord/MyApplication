ee2f3aff39c0e2cfdaf79f85b8cb427c227a9eb7
jfaler <Jfaler@gmail.com>
2020-05-04 13:09:10 -0600

    ??????

--- app/src/main/java/com/chalupa/myapplication/MainActivity.kt
+++ app/src/main/java/com/chalupa/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.chalupa.myapplication
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity as AppCompatActivity1
 
 class MainActivity : AppCompatActivity1() {
@@ -10,6 +11,9 @@ class MainActivity : AppCompatActivity1() {
         setContentView(R.layout.activity_main)
         var jim = "jim"
         var jim2 = "will it work?"
+        val welcomeMessage: TextView = findViewById(R.id.texbox)
+        welcomeMessage.text = jim2
+        
 
     }
 }
