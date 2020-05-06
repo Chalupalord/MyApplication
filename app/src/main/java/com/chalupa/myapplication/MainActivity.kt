13510ac7526b2538427774c50213637a68843d39
jfaler <Jfaler@gmail.com>
2020-05-06 16:00:02 -0600

    ??????

--- app/src/main/java/com/chalupa/myapplication/MainActivity.kt
+++ app/src/main/java/com/chalupa/myapplication/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.chalupa.myapplication
 
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 import androidx.appcompat.app.AppCompatActivity as AppCompatActivity1
 
 class MainActivity : AppCompatActivity1() {
 
+    private val default = "Hello World"
+    private val jim = "Jim"
+    private val jim2 = "Will it work?"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var jim = "jim"
-        var jim2 = "will it work?"
-        val welcomeMessage: TextView = findViewById(R.id.texbox)
-        welcomeMessage.text = jim2
-        
 
+        displayMessage.text = default
+
+    }
+
+    fun pushMePushed(view: View){
+        when(displayMessage.text){
+            default -> displayMessage.text = jim
+            jim ->  displayMessage.text = jim2
+            jim2 -> displayMessage.text = default
+        }
     }
 }
